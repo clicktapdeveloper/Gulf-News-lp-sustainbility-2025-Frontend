cbea7939915bd8d3e754b9fcca569e11d27863be
Fahad Abdul Qayyum <fahad.ocreateo@gmail.com>
2025-10-28 17:32:30 +0500

    Changes in Award, Event, Hero.

--- src/components/Event/index.tsx
+++ src/components/Event/index.tsx
@@ -3,7 +3,9 @@ import CustomButton from "@/screens/CustomButton";
 const Event = () => {
     return <div className="flex flex-col text-center gap-2 py-mobile-padding sm:py-tablet-padding lg:py-desktop-padding bg-[var(--white-color)] space-y-[var(--space-y-mobile)] lg:space-y-[var(--space-y)] px-standard-mobile-padding sm:px-standard-tablet-padding lg:px-standard-desktop-padding 2xl:px-standard-xl-padding">
         <h1 className="text-xl lg:text-title-text-size font-bold !text-[var(--secondary-color)]">About Event</h1>
-        <p className="text-base lg:text-subtitle-text-size">
+        <p className="text-[14px] md:text-[16px] lg:text-[20px]"
+        style={{ fontFamily: "'Nunito Sans', sans-serif" }}
+        >
         The Sustainability Excellence Awards 2025, powered by Gulf News and BeingShe, celebrates pioneers shaping a greener, more resilient future. From renewable energy innovation to circular fashion, this event recognizes excellence across industries championing sustainable progress.
         </p>
         <div className="mx-auto w-full sm:w-1/2 lg:w-1/6">
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
     const navigate = useNavigate();
-    return <div className="relative flex flex-col lg:flex-row h-[600px] overflow-hidden">
-        <div className="py-mobile-padding sm:py-tablet-padding lg:py-desktop-padding px-standard-mobile-padding sm:px-standard-tablet-padding lg:px-standard-desktop-padding 2xl:px-standard-xl-padding z-10 !backdrop-blur-none lg:!backdrop-blur-xs 2xl:backdrop-blur-none !bg-[#EBF1E7]/40 xl:!bg-transparent space-y-[var(--space-y)]">
+    return <div className="relative flex flex-col lg:flex-row h-[620px] sm:h-[600px] overflow-hidden">
+        <div className="py-mobile-padding sm:py-tablet-padding lg:py-desktop-padding px-standard-mobile-padding sm:px-standard-tablet-padding lg:pl-standard-desktop-padding 2xl:px-standard-xl-padding z-10 !backdrop-blur-none lg:!backdrop-blur-xs 2xl:backdrop-blur-none !bg-[#EBF1E7]/40 xl:!bg-transparent space-y-[var(--space-y)]">
             <h1 className='text-[34px] sm:text-[55px] lg:text-[50px] font-[600] leading-tight'>
                 <div className='text-[var(--secondary-color)] w-full whitespace-nowrap'>Celebrating Leaders<span className="text-black">,</span></div>
                 <div className='text-[var(--tertiary-color)] w-full'>Inspiring Change</div>
             </h1>
-                <p className='text-[14px] md:text-[16px] lg:text-[18px] max-w-3xl !font-[650]' style={{ fontFamily: "'Nunito Sans', sans-serif" }}>
+                <p className='text-[14px] md:text-[16px] lg:text-[18px] max-w-lg !font-[650]' style={{ fontFamily: "'Nunito Sans', sans-serif" }}>
                 Join the UAE's most impactful sustainability recognition event — honoring organizations and individuals driving environmental and social transformation.
                 </p>
                 <div className='flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-4'>
@@ -44,7 +44,7 @@ const Hero = () => {
                 </div>
         </div>
         <img src="/hero.svg" alt="Hero" className="absolute top-0 right-0" />
-        <img src="/logo/SEA.svg" alt="Hero" className="absolute top-96 lg:top-0 right-[100px] lg:right-[500px] h-[200px] lg:h-[500px]" />
+        <img src="/logo/SEA.svg" alt="Hero" className="absolute top-96 lg:top-10 right-[100px] lg:right-[500px] h-[200px] lg:h-[350px]" />
     </div>
 }
 
