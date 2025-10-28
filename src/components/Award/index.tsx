463c1dd086419fa87bccb6316a2d5dbcb691e1c5
Fahad Abdul Qayyum <fahad.ocreateo@gmail.com>
2025-10-28 16:15:51 +0500

    Nominate now button with linked in award.

--- src/components/Award/index.tsx
+++ src/components/Award/index.tsx
@@ -1,8 +1,11 @@
 import CustomButton from "@/screens/CustomButton";
 import CardCategory from "@/screens/CardCategory";
 import AutoCarousel from "@/components/AutoCarousel";
+import { useNavigate } from 'react-router-dom';
 
 const Award = () => {
+  const navigate = useNavigate();
+
     return <div className="flex flex-col text-center gap-2 px-standard-mobile-padding sm:px-standard-tablet-padding lg:px-standard-desktop-padding 2xl:px-standard-xl-padding py-mobile-padding sm:py-tablet-padding lg:py-desktop-padding space-y-[var(--space-y)] bg-[url('/layout.svg'),_url('/award.svg')] bg-no-repeat bg-center bg-cover min-h-[400px] sm:min-h-[400px] lg:h-80">
         <h1 className="text-2xl lg:text-title-text-size font-bold !text-white">Award Categories & Criteria</h1>
         <div className="mx-auto w-full max-w-6xl">
@@ -14,7 +17,10 @@ const Award = () => {
             </AutoCarousel>
         </div>
         <div className="mx-auto w-5/6 sm:w-1/2 lg:w-1/6">
-            <CustomButton className="w-full !border-white md:border-0">Register Now</CustomButton>
+            <CustomButton 
+            className="w-full !border-white md:border-0"
+            onClick={() => navigate('/apply-for-nomination')}
+            >Nominate Now</CustomButton>
         </div>
     </div>
 }
