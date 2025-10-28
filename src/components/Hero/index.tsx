1bb35fea021bf97e1b8a1cfd45d2950a2ac98e7f
Fahad Abdul Qayyum <fahad.ocreateo@gmail.com>
2025-10-28 12:11:30 +0500

    Hero section's completed the trnasitioned.

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,49 +4,50 @@ import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
     const navigate = useNavigate();
-    return <div className='flex flex-col lg:flex-row items-center justify-center py-mobile-padding sm:py-tablet-padding lg:py-desktop-padding px-standard-mobile-padding sm:px-standard-tablet-padding lg:px-standard-desktop-padding 2xl:px-standard-xl-padding'>
-        <div className="flex-2 space-y-[var(--space-y)] w-full lg:w-auto">
-            <h1 className='text-[34px] sm:text-[55px] lg:text-[76px] font-[600] leading-tight'>
+    // return <div className="relative flex flex-col lg:flex-row py-mobile-padding sm:py-tablet-padding lg:py-desktop-padding px-standard-mobile-padding sm:px-standard-tablet-padding lg:px-standard-desktop-padding 2xl:px-standard-xl-padding h-[600px]">
+    return <div className="relative flex flex-col lg:flex-row h-[600px] overflow-hidden">
+        <div className="py-mobile-padding sm:py-tablet-padding lg:py-desktop-padding px-standard-mobile-padding sm:px-standard-tablet-padding lg:px-standard-desktop-padding 2xl:px-standard-xl-padding z-10 !backdrop-blur-none lg:!backdrop-blur-xs 2xl:backdrop-blur-none !bg-[#EBF1E7]/40 xl:!bg-transparent space-y-[var(--space-y)]">
+            <h1 className='text-[34px] sm:text-[55px] lg:text-[50px] font-[600] leading-tight'>
                 <div className='text-[var(--secondary-color)] w-full whitespace-nowrap'>Celebrating Leaders<span className="text-black">,</span></div>
                 <div className='text-[var(--tertiary-color)] w-full'>Inspiring Change</div>
             </h1>
-            <p className='text-[14px] md:text-[16px] lg:text-[18px] max-w-2xl' style={{ fontFamily: "'Nunito Sans', sans-serif" }}>
-            Join the UAE's most impactful sustainability recognition event — honoring organizations and individuals driving environmental and social transformation.
-            </p>
-            <div className='flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-4'>
-                <span className='flex items-center gap-2'>
-                    <Calendar className="w-4 h-4 sm:w-5 sm:h-5" />
-                    <p className="text-[16px] lg:text-[20px]" style={{ fontFamily: "'Nunito Sans', sans-serif" }}>26 November 2025</p>
-                </span>
-                <span className='flex items-center gap-2'>
-                    <Clock3 className="w-4 h-4 sm:w-5 sm:h-5" />
-                    <p className="text-[16px] lg:text-[20px]" style={{ fontFamily: "'Nunito Sans', sans-serif" }}>6:00 PM - 8:30 PM</p>
-                </span>
-                <span className='flex items-center gap-2'>
-                    <MapPin className="w-4 h-4 sm:w-5 sm:h-5" />
-                    <p className="text-[16px] lg:text-[20px]" style={{ fontFamily: "'Nunito Sans', sans-serif" }}>Dubai, UAE</p>
-                </span>
-            </div>
-            <div className='grid grid-cols-1 sm:grid-cols-2 !justify-start gap-3 sm:gap-4 w-full xl:w-2/3 pt-5'>
-                <CustomButton variant="outline" 
-                onClick={() => navigate('/apply-for-nomination')} 
-                className="w-full sm:w-auto">Apply for Nomination</CustomButton>
-                <CustomButton variant="outline" 
-                onClick={() => navigate('/inquire-about-sponsorship')} 
-                className="w-full sm:w-auto">Inquire about Sponsorship</CustomButton>
-                <CustomButton 
-                onClick={() => navigate('/register-for-attend')} 
-                className="w-full sm:w-auto">Register for Attend</CustomButton>
-                <span className='flex flex-col justify-center'>
-                    <small className='text-xs text-gray-500 leading-4'>Powered by</small>
-                    <h5 className='font-bold text-[var(--secondary-color)] text-sm sm:text-base'>Gulf News & BeingShe</h5>
-                </span>
-            </div>
+            {/* <span className="space-y-[var(--space-y)]"> */}
+                <p className='text-[14px] md:text-[16px] lg:text-[18px] max-w-3xl !font-[650]' style={{ fontFamily: "'Nunito Sans', sans-serif" }}>
+                Join the UAE's most impactful sustainability recognition event — honoring organizations and individuals driving environmental and social transformation.
+                </p>
+                <div className='flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-4'>
+                    <span className='flex items-center gap-2'>
+                        <Calendar className="w-4 h-4 sm:w-5 sm:h-5" />
+                        <p className="text-[16px] lg:text-[20px] !font-[650]" style={{ fontFamily: "'Nunito Sans', sans-serif" }}>26 November 2025</p>
+                    </span>
+                    <span className='flex items-center gap-2'>
+                        <Clock3 className="w-4 h-4 sm:w-5 sm:h-5" />
+                        <p className="text-[16px] lg:text-[20px] !font-[650]" style={{ fontFamily: "'Nunito Sans', sans-serif" }}>6:00 PM - 8:30 PM</p>
+                    </span>
+                    <span className='flex items-center gap-2'>
+                        <MapPin className="w-4 h-4 sm:w-5 sm:h-5" />
+                        <p className="text-[16px] lg:text-[20px] !font-[650]" style={{ fontFamily: "'Nunito Sans', sans-serif" }}>Dubai, UAE</p>
+                    </span>
+                </div>
+                <div className='grid grid-cols-1 sm:grid-cols-2 !justify-start gap-3 sm:gap-4 w-full xl:w-2/3 pt-5'>
+                    <CustomButton variant="outline" 
+                    onClick={() => navigate('/apply-for-nomination')} 
+                    className="w-full sm:w-auto">Nominate Now</CustomButton>
+                    <CustomButton variant="outline" 
+                    onClick={() => navigate('/inquire-about-sponsorship')} 
+                    className="w-full sm:w-auto">Be a Sponsor</CustomButton>
+                    <CustomButton 
+                    onClick={() => navigate('/register-for-attend')} 
+                    className="w-full sm:w-auto">Register to Attend</CustomButton>
+                    <span className='flex flex-col justify-center'>
+                        <small className='text-xs text-gray-500 leading-4'>Powered by</small>
+                        <h5 className='font-bold text-[var(--secondary-color)] text-sm sm:text-base'>Gulf News & BeingShe</h5>
+                    </span>
+                </div>
+            {/* </span> */}
         </div>
-        <div className="flex-1 relative mt-8 lg:mt-0 w-full max-w-md lg:max-w-none">
-            <img src="/logo/SEA.svg" alt="SEA" className="absolute left-8 sm:left-12 lg:left-16 top-1/2 -translate-x-1/2 -translate-y-1/2 w-16 h-16 sm:w-20 sm:h-20 lg:w-24 lg:h-24 xl:w-32 xl:h-32" />
-            <img src="/hero.svg" alt="Hero" className="w-full h-auto" />
-        </div>
+        <img src="/hero.svg" alt="Hero" className="absolute top-0 right-0" />
+        <img src="/logo/SEA.svg" alt="Hero" className="absolute top-96 lg:top-0 right-[100px] lg:right-[500px] h-[200px] lg:h-[500px]" />
     </div>
 }
 
