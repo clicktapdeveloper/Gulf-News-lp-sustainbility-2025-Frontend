fb1fb8845e5ee3832ab8bd1f39c765ab533d585f
Fahad Abdul Qayyum <fahad.ocreateo@gmail.com>
2025-10-29 13:05:10 +0500

    Hero section's logo size decreased.

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -60,7 +60,7 @@ const Hero = () => {
         {/* Desktop view images - absolute positioned */}
         <img src="/hero.svg" alt="Hero" className="hidden lg:block absolute top-0 right-0" />
         {/* <img src="/logo/SEA.svg" alt="Hero" className="hidden lg:block absolute top-64 lg:top-10 right-[30px] lg:right-[500px] h-[160px] lg:h-[350px]" /> */}
-        <img src="/logo/SEA.svg" alt="Hero" className="hidden lg:block absolute top-64 lg:top-10 right-[30px] lg:right-[30px] h-[160px] lg:h-[350px]" />
+        <img src="/logo/SEA.svg" alt="Hero" className="hidden lg:block absolute top-64 lg:top-10 right-[30px] lg:right-[30px] h-[160px] lg:h-[300px]" />
     </div>
 }
 
